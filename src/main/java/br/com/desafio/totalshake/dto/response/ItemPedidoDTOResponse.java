f959488f1f0da1874631df73c90c1347db18a427
Lukaz Rocha <lrcosta@ciandt.com>
2022-09-09 14:25:47 -0300

    feat:criando DTOs

---
+++ src/main/java/br/com/desafio/totalshake/dto/response/ItemPedidoDTOResponse.java
@@ -0,0 +1,23 @@
+package br.com.desafio.totalshake.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ItemPedidoDTOResponse {
+
+    private Long id;
+
+    private Integer quantidade;
+
+    private String descricao;
+
+    private Long pedidoId;
+
+}
